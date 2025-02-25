153212c1759f160236706d0da98748e96a764b37
chavarreca <sarredondoc1300@alumno.ipn.mx>
2025-02-25 02:22:43 -0600

    Propuesta de Backend PS: No hace nadaaaaa

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hola Culeros, aqui el anticristo2007</h1>')
+});
+
+app.listen(port, () => {
+	console.log("Servidor en puerto 3000...")
+});
+
+
